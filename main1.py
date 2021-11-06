855c5c370429e52d01734d1e81d1c1d623cf1b7d
ovplgn <54017139+ovplgn@users.noreply.github.com>
2021-11-06 03:11:12 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,14 @@
+nums1=[1,2,3,4,5]
+nums2=[0,0,0,0,0]
+
+def medians(nums1, nums2):
+    s=sorted(nums1+nums2)
+    if len(s)==0:
+        m=s
+    elif len(s) %2==1:
+        m = s[len(s)//2]
+    else:
+        m= (s[len(s)//2] + s[len(s)//2 - 1]) /2
+    return float(m)
+
+print(medians(nums1, nums2))
